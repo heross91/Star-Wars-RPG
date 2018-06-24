677a22cc2ba2ccc463b7f2ced05120e4d059b115
heross91 <38013016+heross91@users.noreply.github.com>
2018-06-23 20:06:11 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,32 @@
+    $(document).ready(function(){
+    $(":image").css("background-color", "red");
+});
+    // JavaScript function that wraps everything
+    $(document).ready(function() {
+
+// Array for jedi
+var jedis = [];
+
+// Target jedi
+var targetJedi;
+
+// User"s selection
+var userAnswer;
+
+// Array for jedi
+var jedis = [
+  "Kenobi",
+  "Windu",
+  "Yoda",
+  "Dooku",
+  "Maul",
+  "Vader"
+];
+
+
+
+
+
+});
+
 
